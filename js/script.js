f666dae3bb6bab36d091d0d2d3bfc0471fc8aee6
Artem Grokholskyi <aptem21599@gmail.com>
2023-10-20 17:10:22 +0300

    Comm 4

--- js/script.js
+++ js/script.js
@@ -106,4 +106,40 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
+    //Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]');
+    const modal = document.querySelector('.modal');
+    const modalCloseBtn = document.querySelector('[data-close]');;
+
+    modalTrigger.forEach((btn) => {
+        btn.addEventListener('click', function() {
+            modal.classList.toggle('show');
+            modal.classList.toggle('hide');
+
+            // document.body.style.overflow = 'hidden';
+        })
+    });
+
+    function closeModal() {
+        modal.classList.toggle('show');
+        modal.classList.toggle('hide');
+
+            // document.body.style.overflow = '';
+    };
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal()
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal()
+        }
+    });
+
 });
